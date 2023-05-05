8069b1db4e0dc5f97ba4bfcd6b722296185e7a6a
salvob <tholalak@yahoo.it>
2023-05-05 20:33:36 +0200

    fix: first attempt

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ app = FastAPI()
 
 # define your Elasticsearch connection here
 hostname = os.environ.get('ELASTIC_HOSTNAME')
+user = os.environ.get("ELASTIC_USERNAME")
+password = os.environ.get("ELASTIC_PASSWORD")
 # get environment variable HOSTNAME
-es = Elasticsearch([hostname], timeout=30, max_retries=10, retry_on_timeout=True, username=os.environ.get("ELASTIC_USERNAME"), password=os.environ.get("ELASTIC_PASSWORD"))
+es = Elasticsearch([hostname], verify_certs=False , timeout=30, max_retries=10, retry_on_timeout=True, 
+                   basic_auth=[user, password])
 
 # define your JSON schema here
-class Message(BaseModel):
+class Message(BaseModel, extra="allow"):
     id: str
     message: str
 
 # define your API endpoint here
 @app.post("/messages")
 async def create_message(message: Message):
-    es.index(index='starship_olympics_analytics', id=message.id, body=message.dict())
+    es.index(index='starship_olympics_analytics', #id=message.id,
+              body=message.dict(), op_type='create')
     return {"message": "Message has been indexed successfully."}
