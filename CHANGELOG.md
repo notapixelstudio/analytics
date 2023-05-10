971ca342c80536597ab24b956be394d4a608947a
salvob <tholalak@yahoo.it>
2023-05-10 20:07:48 +0200

    bump: version 0.5.0 → 0.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## v0.6.0 (2023-05-10)
+
+### Feat
+
+- **pipeline**: added pipeline geoip
+
 ## v0.5.0 (2023-05-10)
 
 ### Fix
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from elasticsearch import Elasticsearch
 from fastapi import FastAPI, Header, HTTPException, Request
 from pydantic import BaseModel
 
-app = FastAPI(version="0.5.0", title="SO Analytics", description="SO Analytics API")
+app = FastAPI(version="0.6.0", title="SO Analytics", description="SO Analytics API")
 
 # define your Elasticsearch connection here
 hostname = os.environ.get("ELASTIC_HOSTNAME", "https://localhost:9200")
